3d4dc250c05561e0fc5c6cc989f2d9424331c1cc
zebuti <102354890+zebuti@users.noreply.github.com>
2023-04-24 09:45:04 +0300

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,67 +1,62 @@
 #include "sort.h"
-#include <stdio.h>
-/**
- *swap_node - swap a node for his previous one
- *@node: node
- *@list: node list
- *Return: return a pointer to a node which was enter it
- */
-listint_t *swap_node(listint_t *node, listint_t **list)
-{
-	listint_t *back = node->prev, *current = node;
-	/*NULL, 19, 48, 9, 71, 13, NULL*/
 
-	back->next = current->next;
-	if (current->next)
-		current->next->prev = back;
-	current->next = back;
-	current->prev = back->prev;
-	back->prev = current;
-	if (current->prev)
-		current->prev->next = current;
-	else
-		*list = current;
-	return (current);
-}
+void move_left(listint_t *curr, listint_t *prev, listint_t **head);
+
 /**
- *cocktail_sort_list - this is a cocktail sort implementation
- *working on a double linked lists
- *@list: list
- */
+* cocktail_sort_list - coctail sort in doubly linked list
+* @list: A Doubly linked list
+*/
+
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *node;
-	int swap_done = 1;
+	listint_t *cur;
+	listint_t *max = NULL;
+	listint_t *min = NULL;
 
-	if (list == '\0' || (*list) == '\0' || (*list)->next == '\0')
+	if (!list || !(*list) || (*list)->next == NULL)
 		return;
-	node = *list;
-	while (swap_done == 1)
-	{
-		swap_done = 0;
-		while (node->next)
+	cur = *list;
+	do {
+		while (cur->next)
 		{
-			if (node->n > node->next->n)
-			{
-				node = swap_node(node->next, list);
-				swap_done = 1;
-				print_list(*list);
-			}
-			node = node->next;
-		}
-		if (swap_done == 0)
-			break;
-		swap_done = 0;
-		while (node->prev)
-		{
-			if (node->n < node->prev->n)
-			{
-				node = swap_node(node, list);
-				swap_done = 1;
-				print_list(*list);
-			}
+			if (cur->n > cur->next->n)
+				move_left(cur->next, cur, list);
 			else
-				node = node->prev;
+				cur = cur->next;
 		}
-	}
+		max = cur;
+		while (cur->prev != min)
+		{
+			if (cur->n < cur->prev->n)
+				move_left(cur, cur->prev, list);
+			else
+				cur = cur->prev;
+		}
+		min = cur;
+	} while (min != max);
+}
+
+/**
+* move_left - swaps two members of a list
+*
+* @curr: current node
+* @prev: previous node
+* @head: head of list
+*/
+void move_left(listint_t *curr, listint_t *prev, listint_t **head)
+{
+	listint_t *swap1 = curr->next;
+	listint_t *swap2 = prev->prev;
+
+	if (swap1 != NULL)
+		swap1->prev = prev;
+	if (swap2 != NULL)
+		swap2->next = curr;
+	curr->prev = swap2;
+	prev->next = swap1;
+	curr->next = prev;
+	prev->prev = curr;
+	if (*head == prev)
+		*head = curr;
+	print_list(*head);
 }
